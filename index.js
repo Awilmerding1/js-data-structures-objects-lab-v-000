f2c0150733167915bc4f9c65604a734549197feb
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-08-27 15:25:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your solution in this file!
+
+driver = {};
+
+function updateDriverWithKeyAndValue(driver, key, value) {
+  
+const newDriver = {...driver, address: value};
+
+return newDriver;
+  
+}
